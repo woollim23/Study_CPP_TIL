d90ff0e2c513d4a0033ee23329cfffb37e061033
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-20 16:58:28 +0900

    Title: 숫자의 조건 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231120/숫자의 조건 여부/number's-condition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if(a >= 113){
+        cout << 1;
+    }
+    else{
+        cout << 0;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
