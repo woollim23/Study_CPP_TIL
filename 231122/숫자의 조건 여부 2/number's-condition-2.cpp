911a722b4264748f45cf419ec2fc16c9dd9c999b
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-22 18:13:48 +0900

    Title: 숫자의 조건 여부 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231122/숫자의 조건 여부 2/number's-condition-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if(a == 5){
+        cout << "A";
+    }
+    if((a%2) == 0){
+        cout << "B";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
