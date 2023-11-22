03140b3d15063302a2852e8f8c56dfd9ff25e4e2
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-22 14:01:23 +0900

    Title: 살 수 있는 물건, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231122/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(n >= 3000){
+        cout << "book";
+    }
+    else{
+        cout << "mask";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
