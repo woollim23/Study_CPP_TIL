5d6965e27b631aaa17621a56ee81c27edb5c068b
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-22 12:47:37 +0900

    Title: 2개 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231122/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b;
+
+    c = a > b ? a : b;
+
+    cout << c;
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
