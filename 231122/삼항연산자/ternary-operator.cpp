1805aff4fdff6ec69e6319e34fe1de61a9f43983
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-22 13:47:33 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231122/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    
+    cin >> n;
+
+    s = n == 100 ? "pass" : "failure";
+
+    cout << s;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
