f59589ac1ac8bd9a9b17687065dec628334fde63
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-22 13:56:34 +0900

    Title: 물의 상태, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231122/물의 상태/state-of-water.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(n < 0){
+        cout << "ice";
+    }
+    else if(n >= 100){
+        cout << "vapor";
+    }
+    else{
+        cout << "water";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
