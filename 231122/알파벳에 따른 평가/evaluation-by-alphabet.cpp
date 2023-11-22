93ad5fe58eb39034d032f5fa6bbb943025fdfdd3
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-22 18:02:53 +0900

    Title: 알파벳에 따른 평가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231122/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+
+    if(c == 'S'){
+        cout << "Superior";
+    }
+    else if(c == 'A'){
+        cout << "Exellent";
+    }
+    else if(c == 'B'){
+        cout << "Good";
+    }
+    else if(c == 'C'){
+        cout << "Usually";
+    }
+    else if(c == 'D'){
+        cout << "Effort";
+    }
+    else {
+        cout << "Failure";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
