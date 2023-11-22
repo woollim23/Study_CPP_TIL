489119f63064d15d5448c33ea61fa3606c3f92b6
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-22 17:58:25 +0900

    Title: 출석 부르기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231122/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(n==1){
+        cout << "John";
+    }
+    else if(n==2){
+        cout << "Tom";
+    }
+    else if(n==3){
+        cout << "Paul";
+    }
+    else{
+        cout << "Vacancy";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
