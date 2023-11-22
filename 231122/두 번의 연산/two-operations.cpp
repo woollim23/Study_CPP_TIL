f51e5f785638809abfd2964e2a31191e56c73042
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-22 18:18:44 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231122/두 번의 연산/two-operations.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if((a%2) != 0){
+        a += 3;
+    }
+    if((a%3) == 0){
+        a /= 3;
+    }
+    cout << a;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
