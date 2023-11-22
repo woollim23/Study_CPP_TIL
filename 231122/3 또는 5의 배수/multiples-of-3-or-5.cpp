85cd21a100d3d71cf33d6b4bf6d07e2e5f6b1655
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-22 18:31:53 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231122/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if((a%3) == 0){
+        cout << "YES\n";
+    }
+    else{
+        cout << "NO\n";
+    }
+
+    if((a%5) == 0){
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
