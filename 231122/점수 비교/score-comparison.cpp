96a6578f8ac89e806f32dc25d426500a6e6f9075
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-22 20:09:06 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231122/점수 비교/score-comparison.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a1, a2, b1, b2;
+
+    cin >> a1 >> a2 >> b1 >> b2;
+
+    if((a1 > b1) && (a2 > b2)){
+        cout << 1;
+    }
+    else{
+        cout << 0;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
