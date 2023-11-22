5bb29547b715c64b8e0d42ad17b76ee974c774f9
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-22 18:05:32 +0900

    Title: 살 수 있는 물건 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231122/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(n >= 3000){
+        cout << "book";
+    }
+    else if(n >= 1000){
+        cout << "mask";
+    }
+    else if(n >= 500){
+        cout << "pen";
+    }
+    else{
+        cout << "no";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
