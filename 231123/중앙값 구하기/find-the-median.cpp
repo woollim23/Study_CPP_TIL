af8fb697e4a36cacdeed937993f237a0a778d5fc
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-23 21:07:04 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231123/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(a > b){
+        if(a > c){
+            if(b > c){
+                cout << b;
+            }
+            else{
+                cout << c;
+            }
+        }
+        else{
+            cout << b;
+        }
+    }
+    else{
+        if(b > c){
+            if(a > c){
+                cout << a;
+            }
+            else{
+                cout << c;
+            }
+        }
+        else{
+            cout << a;
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
