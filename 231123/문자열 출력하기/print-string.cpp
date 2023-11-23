91f693fd021fc49f0b7d2a68ec8fb7a0cd41b306
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-23 22:01:45 +0900

    Title: 문자열 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231123/문자열 출력하기/print-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++){
+        cout << "LeebrosCode\n";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
