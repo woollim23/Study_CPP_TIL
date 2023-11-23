3f9aea4ddbf20753888f1c36e75b390173482c1f
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-23 20:42:17 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231123/윤년인가/is-leap-year.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y;
+    cin >> y;
+
+    if((y % 4) == 0){
+        if(((y % 100) != 0) || (((y % 100) == 0) && ((y %400) == 0))){
+            cout << "true";
+        }
+        else{
+            cout << "false";
+        }
+    }
+    else{
+        cout << "false";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
