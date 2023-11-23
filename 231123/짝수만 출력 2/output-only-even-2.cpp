8959afa39ef102984419bb7a254e71ce5edf0665
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-23 21:59:33 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231123/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> b >> a;
+
+    while(b >= a){
+        cout << b << " ";
+        b-=2;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
