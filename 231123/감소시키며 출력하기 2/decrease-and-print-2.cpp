6827ca4db6381d7846ee0b285305fe4494b51e9e
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-23 21:57:38 +0900

    Title: 감소시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231123/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 26;
+
+    while(i >= 10){
+        cout << i << " ";
+        i--;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
