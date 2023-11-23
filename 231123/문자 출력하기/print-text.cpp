bfe4b32618ebdbabe9c6e52315aa733cd0d46bc7
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-23 22:01:04 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231123/문자 출력하기/print-text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+    
+    for(int i = 0; i < 8; i++){
+        cout << c;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
