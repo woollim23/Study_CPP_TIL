a4153c540b4bc082909ec89935ac60142b17ef4c
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-23 22:10:48 +0900

    Title: n번 더하기, Time: 15ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231123/n번 더하기/add-n-times.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, n;
+    cin >> a >> n;
+
+    for(int i = 0; i < n; i++){
+        a += n;
+        cout << a << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
