868d22b991509cd677a7bc6fb86aeca0885ec696
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-24 19:21:37 +0900

    Title: 1부터의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231124/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0;
+
+    for(int i = 1; i <= 100; i++){
+        sum += i;
+        if(sum >= n){
+            cout << i;
+            break;
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
