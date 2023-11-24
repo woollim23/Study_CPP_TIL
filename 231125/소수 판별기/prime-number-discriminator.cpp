6563ff9da03044762d930e16e8fa9919c903039a
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-25 00:57:32 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231125/소수 판별기/prime-number-discriminator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    bool check = false;
+
+    for(int i = 2; i < n; i++){
+        if(n % i == 0) check = true;
+    }
+
+    if(!check) cout << "P";
+    else cout << "C";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
