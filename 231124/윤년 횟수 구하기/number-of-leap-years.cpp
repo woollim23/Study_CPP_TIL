a980699823cd0b98ed6823960bf648b3754d147c
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-24 17:48:01 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231124/윤년 횟수 구하기/number-of-leap-years.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        if((i % 4) == 0){
+            if((i % 100 == 0) && (i % 400 != 0)){
+                break;
+            }
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
