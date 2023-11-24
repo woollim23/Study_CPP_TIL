67475319392f26c420d67a537195201a95a9e442
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-24 16:16:19 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231124/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, cnt = 0;
+
+    for(int i = 0; i < 10; i++){
+        if((i % 2) != 0){
+            cnt++;
+        }
+    }
+    cout << cnt;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
