bc474f6d8d3d22ac0280c6133bc307d51eb86adf
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-26 17:45:39 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231126/별표 출력하기 13/print-start-13.cpp
+++ 231126/별표 출력하기 13/print-start-13.cpp
@@ -5,8 +5,23 @@ int main() {
     int n;
     cin >> n;
 
-    for(int i = 0; i < n; i++){
-        
+    for(int i = 0; i < n*2; i++)
+    {
+        if(i % 2 == 0)
+        {
+            for(int j = (n-i/2); j > 0; j--)
+            {
+                cout << "* ";
+            }
+        }
+        else
+        {
+            for(int j = 0; j < (i/2+1); j++)
+            {
+                cout << "* ";
+            }
+        }
+        cout << endl;
     }
     // 여기에 코드를 작ㅍ성해주세요.
     return 0;
