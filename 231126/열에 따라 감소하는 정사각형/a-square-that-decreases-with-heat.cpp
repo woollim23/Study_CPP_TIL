371b11cc2f9c0a23f6c944d2765206e7523bd095
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-26 19:48:12 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231126/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = n; i > 0; i--)
+    {
+        for(int j = n; j > 0; j--)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
