c6fa2351937e43e7a40b98e59038bd4021412991
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-27 21:14:21 +0900

    Title: 1-9 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231127/1-9 정사각형/1-9-square.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 1;;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            cout << cnt;
+            cnt++;
+            if(cnt > 9) cnt = 1;
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
