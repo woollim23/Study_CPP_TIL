7ee688b37cee9d70a3eb6925eb65055c4951c86e
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-29 20:10:26 +0900

    Title: 좌우 지그재그 출력, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231129/좌우 지그재그 출력/print-lr-zigzag.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int cnt = 1;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(i % 2 == 0)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                cout << cnt << " ";
+                cnt++;
+            }
+            cnt = cnt + n - 1;
+        }
+        else
+        {
+            for(int j = 0; j < n; j++)
+            {
+                cout << cnt << " ";
+                cnt--;
+            }
+            cnt = cnt + n + 1;
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
