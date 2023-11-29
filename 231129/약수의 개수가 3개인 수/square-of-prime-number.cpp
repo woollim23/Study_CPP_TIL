9db378bdf2c6c215f32bcce1cc20b173b79e1283
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-29 22:16:24 +0900

    Title: 약수의 개수가 3개인 수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231129/약수의 개수가 3개인 수/square-of-prime-number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, cnt = 0;
+    cin >> a >> b;
+
+    for(int i = a; i <= b; i++)
+    {
+        int sum = 0;
+        for(int j = 1; j <= i; j++)
+        {
+            if(i % j == 0) sum++;
+        }
+
+        if(sum == 3) cnt++;
+    }
+    cout << cnt;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
