2a9dc02152650217ec152d276246fccba241c0a5
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-29 21:29:15 +0900

    Title: 구구단으로 이루어진 직각 삼각형, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231129/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int j = 1; j <= (n - i + 1); j++)
+        {
+            cout << i << " * " << j << " = " << i*j;
+            if(j == (n - i + 1))
+            {
+                cout << endl;
+            }
+            else
+            {
+                cout << " / ";
+            }
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
