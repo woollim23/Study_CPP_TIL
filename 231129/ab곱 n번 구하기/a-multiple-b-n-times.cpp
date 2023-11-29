d789ac5514fe82b077b6691e40a4dba47e274682
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-29 21:49:25 +0900

    Title: ab곱 n번 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231129/ab곱 n번 구하기/a-multiple-b-n-times.cpp
+++ 231129/ab곱 n번 구하기/a-multiple-b-n-times.cpp
@@ -3,13 +3,17 @@ using namespace std;
 
 int main() {
     int n;
-
+    cin >> n;
     for(int i = 0; i < n; i++)
     {
-        int a, b;
+        int a, b, sum = 1;
         cin >> a >> b;
 
-        cout << a * b << endl;
+        for(int j = a; j <= b; j++)
+        {
+            sum *= j;
+        }
+        cout << sum << endl;
     }
     // 여기에 코드를 작성해주세요.
     return 0;
