c7071b44ae445bde04e4e8031e462b9521e1f249
woollim <137277221+woollim23@users.noreply.github.com>
2023-11-30 16:01:08 +0900

    Title: 배열에서 원소의 특징, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231130/배열에서 원소의 특징/characteristics-of-elements-in-an-array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+
+    for(int i = 0; i < 10; i++)
+    {
+        cin >> arr[i];
+
+        if(arr[i] % 3 == 0)
+        {
+            cout << arr[i-1];
+            break;
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
