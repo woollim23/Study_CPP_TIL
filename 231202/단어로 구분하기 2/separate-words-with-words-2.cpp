88b6eaf1e4d40a62762cc0d50e4bf9e4a95f9b6d
woollim <137277221+woollim23@users.noreply.github.com>
2023-12-02 11:01:55 +0900

    Title: 단어로 구분하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231202/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s[10];
+
+    for(int i = 0; i < 10; i++)
+    {
+        cin >> s[i];
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+        if(i%2 == 0) cout << s[i] << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
