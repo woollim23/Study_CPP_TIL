ca3232248a51175063d0e9b65c4e41e0a44950c5
woollim <137277221+woollim23@users.noreply.github.com>
2023-12-06 22:40:07 +0900

    Title: 첫 번째와 두 번째 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231206/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
+++ 231206/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main() {
     string s;
-    char c;
+    char c, b;
     cin >> s;
     
     c = s[0];
-
+    b = s[1];
     for(int i = 0; i < s.length(); i++)
     {
-        if(c == s[i]) s[i] = s[1];
+        if(c == s[i]) s[i] = b;
+        else if(b == s[i]) s[i] = c;
     }
-    s[1] = c;
 
     cout << s;
     // 여기에 코드를 작성해주세요.
