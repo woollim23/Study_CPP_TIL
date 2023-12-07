f560351a8c630cc5ba82959c76f91ef44839f16a
woollim <137277221+woollim23@users.noreply.github.com>
2023-12-07 21:21:41 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231207/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> n;
+        cout << (char)n << " ";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
