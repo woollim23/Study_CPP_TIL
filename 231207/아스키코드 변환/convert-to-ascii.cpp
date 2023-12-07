66aa5dc75a9a6204d641a0b12b608f768d9ab827
woollim <137277221+woollim23@users.noreply.github.com>
2023-12-07 21:23:55 +0900

    Title: 아스키코드 변환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231207/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char a;
+    int b;
+    cin >> a >> b;
+
+    cout << (int)a << " " << (char)b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
