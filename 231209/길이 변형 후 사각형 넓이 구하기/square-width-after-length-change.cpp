736e2b04add9218c356d6d4dc7c9b3ea1ad22ac3
woollim <137277221+woollim23@users.noreply.github.com>
2023-12-09 23:56:21 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231209/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >>b;
+    cout << a+8 <<endl << b*3 <<endl<<(a+8)*(b*3);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
