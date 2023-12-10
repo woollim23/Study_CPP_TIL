3b878aebeb96fb7f8405085381ec3fec20efdc51
woollim <137277221+woollim23@users.noreply.github.com>
2023-12-10 23:12:26 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231210/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void print()
+{
+    cout << "**********\n";
+}
+
+int main() {
+    for(int i = 0; i < 5; i++)
+    {
+        print();
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
