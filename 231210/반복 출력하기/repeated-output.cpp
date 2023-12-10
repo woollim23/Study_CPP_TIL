6a36023028ad3baa57f3cdd6b0ae8cd44bb53c32
woollim <137277221+woollim23@users.noreply.github.com>
2023-12-10 23:13:40 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231210/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void print(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        cout << "12345^&*()_\n";
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    print(n);
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
