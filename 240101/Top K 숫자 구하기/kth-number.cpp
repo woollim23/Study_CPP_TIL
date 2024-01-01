206c558840ac19af7b0620b078402cd723ec76ce
woollim <137277221+woollim23@users.noreply.github.com>
2024-01-01 23:27:54 +0900

    Title: Top K 숫자 구하기, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240101/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    string arr[100];
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    cout << arr[k-1];
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
