3129f229522f202c2d426d3303e9f7217dafec20
woollim <137277221+woollim23@users.noreply.github.com>
2024-01-03 17:53:13 +0900

    Title: 줄 세우기 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240103/줄 세우기 2/line-up-students-2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+
+#define max 1000
+
+using namespace std;
+
+class Test
+{
+    public:
+        int hei;
+        int wei;
+        int num;
+
+        Test(int hei, double wei, int num)
+        {
+            this->hei = hei;
+            this->wei = wei;
+            this->num = num;
+        }
+        Test(){}
+};
+
+Test test[max];
+
+bool Cmp(Test a, Test b)
+{
+    if(a.hei == b.hei)
+    {
+        return a.wei > b.wei;
+    }
+    return a.hei < b.hei;
+}
+
+int main() {
+    int hei;
+    int wei;
+
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> hei >> wei;
+
+        test[i] = Test(hei, wei, i+1);
+    }
+
+    sort(test, test + n, Cmp); 
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << test[i].hei << " " << test[i].wei << " " << test[i].num << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
