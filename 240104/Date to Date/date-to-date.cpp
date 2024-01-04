75063b0a71f8b69c5f0d36571540aaf06b5c9662
woollim <137277221+woollim23@users.noreply.github.com>
2024-01-04 17:17:41 +0900

    Title: Date to Date, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240104/Date to Date/date-to-date.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int a, b, a2, b2;
+    cin >> a >> b >> a2 >> b2;
+
+    int sum = 0;
+
+    while(1)
+    {
+        if(a == a2 && b == b2)
+            break;
+        sum++;
+        
+        if(b <= month[a])
+        {
+            b++;
+        }
+        else
+        {
+            b = 1;
+            a++;
+        }
+    }
+
+    cout << sum;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
