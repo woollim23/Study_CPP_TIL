e3e8fddd6c244655f071b30f46c87a9393adea47
woollim <137277221+woollim23@users.noreply.github.com>
2024-01-08 22:43:13 +0900

    Title: 연속되는 수 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240108/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 1000
+using namespace std;
+
+int main() {
+    int arr[MAX];
+    int n, max = 0, cnt = 1;
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        if(i == 0 || arr[i-1] >= arr[i])
+        {
+            cnt = 1;
+        }
+        else
+            cnt++;
+        if(cnt > max) max = cnt;
+    }
+
+    cout << max;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
