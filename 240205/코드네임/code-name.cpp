e8020be03f302ed2e1e8f5d71f2b04620b79ff31
woollim <137277221+woollim23@users.noreply.github.com>
2024-02-05 18:00:26 +0900

    Title: 코드네임, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240205/코드네임/code-name.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Test
+{
+public:
+    char code;
+    int score;
+
+    Test(char code, int score)
+    {
+        this->code = code;
+        this->score = score;
+    }
+    Test(){}
+};
+
+int main()
+{
+    int min = 100, index;
+    Test test[5];
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> test[i].code >> test[i].score;
+
+        if(min > test[i].score)
+        {
+            min = test[i].score;
+            index = i;
+        }
+    }
+
+    cout << test[index].code << " " << test[index].score;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
