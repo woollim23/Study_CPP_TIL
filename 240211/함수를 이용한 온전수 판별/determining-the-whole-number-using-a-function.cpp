1b41265e6729915579e1ab577af6dd8bc9239fa7
woollim <137277221+woollim23@users.noreply.github.com>
2024-02-11 23:25:30 +0900

    Title: 함수를 이용한 온전수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240211/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool Test(int i)
+{
+    if((i % 2 == 0) || (i % 10 == 5) || ((i % 3 == 0)&&(i % 9 != 0)))
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int main() {
+    int a, b, cnt = 0;
+    cin >> a >> b;
+
+    for(int i = a; i <= b; i++)
+    {
+        if(Test(i)) cnt++;
+    }
+    cout << cnt;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
