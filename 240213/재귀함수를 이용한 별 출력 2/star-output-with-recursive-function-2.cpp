c9abef03e305a21c7e1dda83caed0bf9ecc376d2
woollim <137277221+woollim23@users.noreply.github.com>
2024-02-13 22:22:43 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240213/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void Test(int n)
+{
+    if(n == 0) return;
+
+    for(int i = 0; i < n; i++)
+        cout << "* ";
+    cout << endl;
+    
+    Test(n - 1);
+    
+    for(int i = 0; i < n; i++)
+        cout << "* ";
+    cout << endl;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    Test(n);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
