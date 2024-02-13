01abb2fca9c07c37aa1a7c47ce9ca556362316a3
woollim <137277221+woollim23@users.noreply.github.com>
2024-02-13 21:49:35 +0900

    Title: 나누고 빼면서 합하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240213/나누고 빼면서 합하기/divide-and-subtract-and-add-up.cpp
+++ 240213/나누고 빼면서 합하기/divide-and-subtract-and-add-up.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int a[100] = {};
-    int n, m, sum = 0;
-    cin >> n >> m;
+int a[100] = {};
+
+int Test(int n, int m)
+{
+    int sum = 0;
 
     for(int i = 0; i < n; i++)
         cin >> a[i];
@@ -19,7 +20,15 @@ int main() {
             m -= 1;
     }
 
-    cout << sum + a[m-1];
+    return sum + a[m - 1];
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+
+    cout << Test(n, m);
     // 여기에 코드를 작성해주세요.
     return 0;
 }
