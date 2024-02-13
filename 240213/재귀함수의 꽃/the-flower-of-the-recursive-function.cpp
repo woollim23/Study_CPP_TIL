396a67d554e0aad14d47cc6e819ce87f34d38f1d
woollim <137277221+woollim23@users.noreply.github.com>
2024-02-13 22:21:32 +0900

    Title: 재귀함수의 꽃, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240213/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void Test(int n)
+{
+    if(n == 0) return;
+
+    cout << n << " ";
+
+    Test(n - 1);
+    
+    cout << n << " ";
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    Test(n);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
