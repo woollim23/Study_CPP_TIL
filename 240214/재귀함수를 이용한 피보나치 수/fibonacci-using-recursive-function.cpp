3e2c9f9ae789c5b727d6d0cb617a68f58e739094
woollim <137277221+woollim23@users.noreply.github.com>
2024-02-14 23:48:13 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240214/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int Test(int n)
+{
+    if(n == 1 || n == 2)
+        return 1;
+    else
+        return Test(n-1) + Test(n-2);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << Test(n);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
