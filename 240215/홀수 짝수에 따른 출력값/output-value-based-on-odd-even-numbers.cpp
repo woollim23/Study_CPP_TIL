6ed2c82acaefcc532d0540464a98cb461ceaf7e6
woollim <137277221+woollim23@users.noreply.github.com>
2024-02-15 21:17:24 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240215/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.cpp
+++ 240215/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int Test(int n) // 짝수 함수
+int Test(int n)
 {
+    if(n == 1)
+        return 1;
     if(n == 2)
         return 2;
 
     return n + Test(n-2);
 }
 
-int Test2(int n) // 홀수 함수
-{
-    if(n == 1)
-        return 1;
-
-     return n + Test2(n-2); 
-}
-
 int main() {
     int n;
     cin >> n;
 
-    if(n % 2 == 0)
-        cout << Test(n);
-    else
-        cout << Test2(n);
+    cout << Test(n);
     // 여기에 코드를 작성해주세요.
     return 0;
 }
