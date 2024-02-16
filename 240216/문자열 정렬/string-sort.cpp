f375030fc59335ec0d2bfeb4376aa79593d6b4d2
woollim <137277221+woollim23@users.noreply.github.com>
2024-02-16 23:56:12 +0900

    Title: 문자열 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240216/문자열 정렬/string-sort.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    sort(s.begin(), s.end());
+
+    cout << s;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
