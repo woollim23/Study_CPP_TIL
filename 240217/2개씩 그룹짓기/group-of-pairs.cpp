df99372001883c88042ede6bc58d6c5289b866ce
woollim <137277221+woollim23@users.noreply.github.com>
2024-02-17 23:45:29 +0900

    Title: 2개씩 그룹짓기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240217/2개씩 그룹짓기/group-of-pairs.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[2000];
+    int n, max = 0;
+    cin >> n;
+
+    for(int i = 0; i < (n * 2); i++)
+        cin >> arr[i];
+
+    sort(arr, arr + n * 2);
+
+    for(int i = 0; i < n; i++)
+    {
+        if((arr[i] + arr[2*n-i-1]) > max)
+            max = (arr[i] + arr[2*n-i-1]);
+    }
+
+    cout << max;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
