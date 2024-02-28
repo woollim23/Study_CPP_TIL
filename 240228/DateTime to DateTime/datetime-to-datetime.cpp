7836524e07e66546a855682fb69fc5a19420d100
woollim <137277221+woollim23@users.noreply.github.com>
2024-02-28 17:56:37 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240228/DateTime to DateTime/datetime-to-datetime.cpp
+++ 240228/DateTime to DateTime/datetime-to-datetime.cpp
@@ -2,38 +2,14 @@
 using namespace std;
 
 int main() {
-    int a1 = 11, b1 = 11, c1 = 11, sum = 0;
     int a2, b2, c2;
     cin >> a2 >> b2 >> c2;
+    
+    int sum = ((a2 * 1440) + (b2 * 60) + c2) - 16511;
 
-
-    //15,840 + 660 + 11
-    if (16511 > ((a2 * 1440) + (b2 * 60) + c2))
-        cout << -1;
-    else
-    {
-
-        while (true)
-        {
-            if (a1 == a2 && b1 == b2 && c1 == c2)
-                break;
-
-            sum++;
-            c1++;
-            if (c1 > 60)
-            {
-                b1++;
-                c1 = 1;
-
-                if (b1 > 24)
-                {
-                    a1++;
-                    b1 = 1;
-                }
-            }
-        }
-
+    if(sum >= 0)
         cout << sum;
-    }
+    else
+        cout << -1;
     return 0;
 }
