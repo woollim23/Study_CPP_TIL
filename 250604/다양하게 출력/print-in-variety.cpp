672b84c7223dc8c26424c31c6773f8197407a238
woollim <137277221+woollim23@users.noreply.github.com>
2025-06-04 23:53:15 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250604/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Total days in Year";
+    cout << endl;
+    cout << 365;
+    cout << endl;
+    cout << "Circumference rate";
+    cout << endl;
+    cout << "3.1415926535";
+    cout << endl;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
